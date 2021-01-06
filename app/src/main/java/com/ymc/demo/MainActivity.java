c639cd11af1e3b031032f903eb0e42a9ecd15378
杨明川 <yangmingchuan@021.com>
2021-01-06 15:55:32 +0800

    Revert "【demo】第二笔错误提交"
    This reverts commit c51553388a598a313780f01adbdeabf88ae906a1.

--- app/src/main/java/com/ymc/demo/MainActivity.java
+++ app/src/main/java/com/ymc/demo/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ShareFileUtils.shareImageToWX(this,"/storage/emulated/0/tupian_out.jpg");
-        ShareFileUtils.shareImageToWX(this,"/storage/emulated/0/1.jpg");
+        //ShareFileUtils.shareImageToWX(this,"/storage/emulated/0/1.jpg");
     }
 }
